8a78e091009c5e058874c72a60eb5f16accac80a
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-02-16 23:55:53 +0000

    Override the default Manager class

--- users_app/models.py
+++ users_app/models.py
@@ -3,7 +3,27 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Overriding the default BaseUser Manager class"""
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError("Email cannot be blank")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user 
+
+
+    def create_superuser(self, email, first_name, last_name, password):
+        user = self.create_user(email, first_name, last_name, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user 
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100)
@@ -11,6 +31,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_verified = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateField(auto_now_add=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['first_name', 'last_name']
