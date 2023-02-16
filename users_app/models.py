4f2652a0ac6090b6857a19341ac9c858f5ce529c
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-02-16 23:49:57 +0000

    Override default user

--- users_app/models.py
+++ users_app/models.py
@@ -1,5 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_verified = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta: 
+        ordering = ["-date_joined"]
+
